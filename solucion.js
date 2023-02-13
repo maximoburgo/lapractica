0a224f2989719f18d394ae7b32bb65f3d454c769
maximoburgo <maximoburgo@gmail.com>
2023-02-13 19:20:26 -0300

    este es el primer commit perro

---
+++ solucion.js
@@ -0,0 +1,22 @@
+function openSolutionWindow(imageSrc) {
+  var newWindow = window.open("", "Solución", "width=400,height=400");
+  newWindow.document.write('<img src="' + imageSrc + '" alt="Solución">');
+  }
+  function openSolutionWindow1() {
+    var newWindow = window.open("", "Solución", "width=400,height=400");
+    newWindow.document.write('<img src="imagen/problema1.png" alt="Solución 1">');
+  }
+  
+  function openSolutionWindow2() {
+    var newWindow = window.open("", "Solución", "width=400,height=400");
+    newWindow.document.write('<img src="imagen/problema2.png" alt="Solución 2">');
+  }
+  
+  function openSolutionWindow3() {
+    var newWindow = window.open("", "Solución", "width=400,height=400");
+    newWindow.document.write('<img src="imagen/problema3.png" alt="Solución 3">');
+  }  
+  function openSolutionWindow4() {
+    var newWindow = window.open("", "Solución", "width=500,height=400");
+    newWindow.document.write('<img src="imagen/problema4.png" alt="Solución 4">');
+  }  
